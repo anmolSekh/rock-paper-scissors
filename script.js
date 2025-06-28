eba4b91954062474fb5628a665645528f60db01a
Anmol <anmol1314@hotmail.com>
2025-06-28 02:41:03 -0700

    computer choice working

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // console.log("Hello World");
 function getComputerChoice() {
-    let option = Math.floor(Math.random*3);
+    let option = Math.floor(Math.random()*3);
     let choice = "";
     switch (option) {
         case(0):
@@ -13,5 +13,12 @@ function getComputerChoice() {
             choice = "scissors";
             break;
     }
+    // console.log(Math.random() * 3);
     return choice;
 }
+
+function  getHumanChoice() {
+
+}
+
+console.log(getComputerChoice());
