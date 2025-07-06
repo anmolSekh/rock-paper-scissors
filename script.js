ee2c0366d38ee40cc40e8f6c89f3008a127e9403
Anmol <anmol1314@hotmail.com>
2025-07-06 05:11:32 -0700

    added playable rounds and human choice

--- script.js
+++ script.js
@@ -18,7 +18,38 @@ function getComputerChoice() {
 }
 
 function  getHumanChoice() {
-
+    let hc = prompt("Choose from rock papers scissors");
+    return (hc.toLowerCase());
 }
 
-console.log(getComputerChoice());
+function playRound(humanChoice, computerChoice) {
+    console.log("You: ", humanChoice);
+    console.log("Computer: ", computerChoice);
+    if(humanChoice=="rock" & computerChoice=="paper" | humanChoice=="paper" & computerChoice=="scissors" | humanChoice=="scissors" & computerChoice=="rock") {
+        console.log("You lose! ", computerChoice, " beats ", humanChoice);
+        return(0);
+    } else if(humanChoice == computerChoice) {
+        console.log("Draw!");
+        return(1);
+    } else {
+        console.log("You win! ", humanChoice, " beats ", computerChoice);
+        return(2);
+    }
+}
+
+let humanScore = 0;
+let computerScore = 0;
+// let cc = getComputerChoice()
+// let hc = getHumanChoice();
+// console.log("Computer: ", cc);
+// console.log("You: ", hc);
+
+let score = playRound(getHumanChoice(), getComputerChoice());
+
+if(score == 0) {
+    computerScore++;
+} else if (score== 2){
+    humanScore++;
+} 
+
+
